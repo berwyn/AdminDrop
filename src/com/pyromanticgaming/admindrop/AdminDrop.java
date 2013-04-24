eef5c8c90a927420aca8848e3a94956232230fbd
unknown <pyropyro78@gmail.com>
2013-04-24 02:46:45 -0700

    Removed console debug line.

--- src/com/pyromanticgaming/admindrop/AdminDrop.java
+++ src/com/pyromanticgaming/admindrop/AdminDrop.java
@@ -3,20 +3,27 @@ package com.pyromanticgaming.admindrop;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class AdminDrop extends JavaPlugin {
+public final class AdminDrop extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
 		getLogger().info("AdminDrop has been enabled.");
 		for(Player player: getServer().getOnlinePlayers()) {
-			if(player.hasPermission("nd.allow")) {
+			if(player.hasPermission("AdminDrop.nd") || player.isOp()) {
 				player.sendMessage("AdminDrop has Reloaded.");
 			}
 		}
-		getCommand("basic").setExecutor(new AdminDropCommandExecutor(this));
+		
+		getServer().getPluginManager().registerEvents(this, this);
+		
+		
+		getCommand("nd").setExecutor(new AdminDropCommandExecutor(this));
 	}
 	
 	@Override
@@ -25,16 +32,20 @@ public final class AdminDrop extends JavaPlugin {
 	}
 	
 	@EventHandler
-	public void onPlayerDeath(PlayerDeathEvent e) {
-	Entity ent = e.getEntity();
-	Player player = (Player) ent;
-	if(!(ent instanceof Player)) {
-		return;
-	} else {
-		if(/*PLACEHOLDER*/AdminDropCommandExecutor.value) {
-			
-		}
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		AdminDropCommandExecutor.dropless.remove(player.getName());
 	}
-	 
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerDeath(PlayerDeathEvent event) {
+	Entity e = event.getEntity();
+	Player p = (Player) e;
+	getLogger().info(p.getName() + " was here");
+	if((e instanceof Player) && AdminDropCommandExecutor.dropless.contains(p.getName())) {
+		event.getDrops().clear();
 	}
+	}
+
+	
 }
--- src/com/pyromanticgaming/admindrop/AdminDropCommandExecutor.java
+++ src/com/pyromanticgaming/admindrop/AdminDropCommandExecutor.java
@@ -1,20 +1,23 @@
 package com.pyromanticgaming.admindrop;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityDeathEvent;
 
 
 
 
 public class AdminDropCommandExecutor implements CommandExecutor {
-
-	//Temp Placeholder
-	public static boolean value = true;
 	
 	private AdminDrop admindrop;
+	
+	public static Set<String> dropless = new HashSet<String>();
+	
 	
 	public AdminDropCommandExecutor(AdminDrop admindrop) {
 		
@@ -24,11 +27,15 @@ public class AdminDropCommandExecutor implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
-		if(cmd.getName().equalsIgnoreCase("ndon") && args.length == 1) {
+		if(cmd.getName().equalsIgnoreCase("nd")) {
 			if (sender instanceof Player) {
-				Player player = (Player) sender;
-				if(player.hasPermission("nd.allow")) {
-					//ADD SHIT HERE
+				if(sender.hasPermission("AdminDrop.nd") || sender.isOp()) {
+					if (args.length > 1) {
+						sender.sendMessage("AdminDrop - Too many arguments!");
+						return false;
+					}
+					DropCommand(sender);
+					return true;
 				} else {
 					sender.sendMessage("AdminDrop - UNAUTHORIZED USER DETECTED!");
 					return false;
@@ -38,10 +45,25 @@ public class AdminDropCommandExecutor implements CommandExecutor {
 				return false;
 			}
 		}
-		if (args.length > 1) {
-			sender.sendMessage("AdminDrop - Too many arguments!");
-			return false;
-		}
 		return false;
+	}
+	
+	private void DropCommand(CommandSender sender) {
+		Player player = (Player) sender;
+		if(!dropless.contains(player.getName())) {
+			DisableDrops(player);
+		} else {
+			EnableDrops(player);
+		}
+	}
+	
+	private void DisableDrops(Player player) {
+		player.sendMessage(ChatColor.DARK_BLUE + "Your items are safe.");
+		dropless.add(player.getName());
+	}
+	
+	private void EnableDrops(Player player) {
+		player.sendMessage(ChatColor.DARK_BLUE + "Your items are not safe.");
+		dropless.remove(player.getName());
 	}
 }
