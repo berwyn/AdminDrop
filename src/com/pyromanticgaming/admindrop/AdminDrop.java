58967783a5ff9fdc00f0bb5dd6c97b1b15376bda
unknown <xxamplifydxx@gmail.com>
2013-04-26 05:00:26 -0400

    Moved files around.

---
+++ src/com/pyromanticgaming/admindrop/AdminDrop.java
@@ -0,0 +1,60 @@
+package com.pyromanticgaming.admindrop;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdminDrop extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("AdminDrop has been enabled.");
+		
+		getServer().getPluginManager().registerEvents(this, this);
+		
+		
+		getCommand("ad").setExecutor(new AdminDropCommandExecutor(this));
+	}
+	
+	@Override
+	public void onDisable() {
+		for(Player player: getServer().getOnlinePlayers()) {
+			if(AdminDropCommandExecutor.dropless.contains(player)) {
+				player.sendMessage(ChatColor.DARK_BLUE + "Your items are not safe.");
+				player.sendMessage("AdminDrop is reloading.");
+				AdminDropCommandExecutor.dropless.remove(player.getName());
+			}
+		}
+		getLogger().info("AdminDrop has been disabled.");
+	}
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		AdminDropCommandExecutor.dropless.remove(player.getName());
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onPlayerDeath(PlayerDeathEvent event) {
+	Entity e = event.getEntity();
+	Player p = (Player) e;
+	if((e instanceof Player) && AdminDropCommandExecutor.dropless.contains(p.getName())) {
+		event.getDrops().clear();
+	}
+	}
+	@EventHandler
+	public void onPlayerDropItem(PlayerDropItemEvent event) {
+	Player p = event.getPlayer();
+	if ((p instanceof Player) && AdminDropCommandExecutor.throwless.contains(p.getName())) {
+		event.setCancelled(true);
+	}
+
+	}
+}
